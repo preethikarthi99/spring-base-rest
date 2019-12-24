f15f625fef00c667c3ba6b111e9d6cfeeb1ed8fa
preethikarthi99 <preethikarthikeyan99@gmail.com>
2019-12-24 18:15:05 +0530

    basic program of spring rest added

---
+++ spring-base-rest-1/src/main/java/org/tact/base/SpringBaseRest1Application.java
@@ -0,0 +1,13 @@
+package org.tact.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaseRest1Application.class, args);
+	}
+
+}
